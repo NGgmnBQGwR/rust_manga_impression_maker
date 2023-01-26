30d0f69ab8d4506bfcd0805dd548ac98eacee46d
0xffffffff <0xgggggg@gmail.com>
2023-01-26 15:37:03 +0700

    Split backend logic from GUI
    This allows to use pure async in certain parts, without using
      lots of block_on() calls.
    Communication is done via channels.
    Using crossbeam because it allows for MPMC, not just MPSC.
    Style - capitalize first letter in comments.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
     clippy::nursery,
     clippy::cargo
 )]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // Hide console window on Windows in release
 
 use anyhow::Context;
 use anyhow::Result as AnyResult;
@@ -16,6 +16,10 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
 type SqlitePool = Pool<sqlx::sqlite::Sqlite>;
+type GuiChannelSend = crossbeam::channel::Sender<GuiCommand>;
+type GuiChannelRecv = crossbeam::channel::Receiver<GuiCommand>;
+type BackendChannelSend = crossbeam::channel::Sender<BackendCommand>;
+type BackendChannelRecv = crossbeam::channel::Receiver<BackendCommand>;
 
 #[derive(Debug, Clone)]
 struct MangaGroup {
@@ -23,28 +27,24 @@ struct MangaGroup {
     id: i64,
 }
 impl MangaGroup {
-    fn delete_cascade(&self, runtime: &tokio::runtime::Runtime, db: &SqlitePool) {
-        let group_entries = runtime
-            .block_on(
-                sqlx::query_as!(
-                    MangaEntry,
-                    r"SELECT * FROM manga_entries WHERE manga_group = ?",
-                    self.id
-                )
-                .fetch_all(db),
-            )
-            .unwrap();
+    async fn delete_cascade(&self, db: &SqlitePool) {
+        let group_entries = sqlx::query_as!(
+            MangaEntry,
+            r"SELECT * FROM manga_entries WHERE manga_group = ?",
+            self.id
+        )
+        .fetch_all(db)
+        .await
+        .unwrap();
 
         for entry in group_entries {
-            entry.delete_cascade(runtime, db);
+            entry.delete_cascade(db).await;
         }
 
-        runtime
-            .block_on(
-                sqlx::query(r"DELETE FROM manga_groups WHERE id = ?")
-                    .bind(self.id)
-                    .execute(db),
-            )
+        sqlx::query(r"DELETE FROM manga_groups WHERE id = ?")
+            .bind(self.id)
+            .execute(db)
+            .await
             .unwrap();
     }
 }
@@ -57,24 +57,20 @@ struct MangaEntry {
     id: i64,
 }
 impl MangaEntry {
-    fn delete_cascade(&self, runtime: &tokio::runtime::Runtime, db: &SqlitePool) {
-        let manga_images = runtime
-            .block_on(
-                sqlx::query_as!(Image, r"SELECT * FROM images WHERE manga = ?", self.id)
-                    .fetch_all(db),
-            )
+    async fn delete_cascade(&self, db: &SqlitePool) {
+        let manga_images = sqlx::query_as!(Image, r"SELECT * FROM images WHERE manga = ?", self.id)
+            .fetch_all(db)
+            .await
             .unwrap();
 
         for image in manga_images {
-            image.delete_cascade(runtime, db);
+            image.delete_cascade(db).await;
         }
 
-        runtime
-            .block_on(
-                sqlx::query(r"DELETE FROM manga_entries WHERE id = ?")
-                    .bind(self.id)
-                    .execute(db),
-            )
+        sqlx::query(r"DELETE FROM manga_entries WHERE id = ?")
+            .bind(self.id)
+            .execute(db)
+            .await
             .unwrap();
     }
 }
@@ -85,15 +81,13 @@ struct Image {
     id: i64,
 }
 impl Image {
-    fn delete_cascade(&self, runtime: &tokio::runtime::Runtime, db: &SqlitePool) {
-        // TODO: delete file
+    async fn delete_cascade(&self, db: &SqlitePool) {
+        // TODO: Delete file from the disk as well
 
-        runtime
-            .block_on(
-                sqlx::query(r"DELETE FROM images WHERE id = ?")
-                    .bind(self.id)
-                    .execute(db),
-            )
+        sqlx::query(r"DELETE FROM images WHERE id = ?")
+            .bind(self.id)
+            .execute(db)
+            .await
             .unwrap();
     }
 }
@@ -101,6 +95,20 @@ impl Image {
 struct ImageCacheEntry {
     file_contents: Vec<u8>,
     thumbnail: Vec<u8>,
+}
+#[derive(Debug)]
+enum GuiCommand {
+    UpdateMangaGroups,
+    CreateNewMangaGroup,
+    GetUpdatedMangaGroups,
+    DeleteMangaGroup(MangaGroup),
+    DeleteMangaEntry(MangaEntry),
+    DeleteImage(Image),
+    Exit,
+}
+#[derive(Debug)]
+enum BackendCommand {
+    UpdateGroups(Vec<MangaGroup>),
 }
 
 fn main() -> AnyResult<()> {
@@ -111,36 +119,24 @@ fn main() -> AnyResult<()> {
         ..Default::default()
     };
 
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .context("Failed to create Tokio runtime.")
-        .unwrap();
+    let (backend_send, backend_recv) = crossbeam::channel::bounded(100);
+    let (gui_send, gui_recv) = crossbeam::channel::bounded(100);
+    let backend_thread = std::thread::spawn(move || DataStorage::start(backend_send, gui_recv));
 
-    let db_pool = runtime
-        .block_on(MangaUI::init_db())
-        .context("Failed to initialize DB pool.")
-        .unwrap();
-
-    let mut manga_ui = MangaUI {
+    let manga_ui = MangaUI {
         manga_groups: Vec::new(),
         selected_group: Option::None,
         group_to_delete: Option::None,
-        images_cache: HashMap::with_capacity(100),
-        cwd: std::env::current_dir()
-            .context("Unable to get CWD.")
-            .unwrap(),
-        db_pool: db_pool,
-        async_runtime: runtime,
+        backend_recv,
+        gui_send,
     };
-
-    manga_ui.refresh_manga_groups();
 
     eframe::run_native(
         "Manga impression maker",
         options,
         Box::new(|_cc| Box::new(manga_ui.setup(_cc))),
     );
+    backend_thread.join().unwrap();
 
     Ok(())
 }
@@ -148,14 +144,122 @@ struct MangaUI {
     manga_groups: Vec<MangaGroup>,
     selected_group: Option<MangaGroup>,
     group_to_delete: Option<MangaGroup>,
+    backend_recv: BackendChannelRecv,
+    gui_send: GuiChannelSend,
+}
+struct DataStorage {
+    manga_groups: Vec<MangaGroup>,
+    selected_group: Option<MangaGroup>,
+    group_to_delete: Option<MangaGroup>,
     images_cache: HashMap<PathBuf, ImageCacheEntry>,
     cwd: PathBuf,
     db_pool: SqlitePool,
-    async_runtime: tokio::runtime::Runtime,
+    backend_send: BackendChannelSend,
+    gui_recv: GuiChannelRecv,
+    exiting: bool,
+}
+
+impl DataStorage {
+    fn start_backend(self, runtime: tokio::runtime::Runtime) {
+        runtime.block_on(self.run());
+    }
+
+    pub async fn run(mut self) {
+        self.update_manga_groups().await;
+        self.send_updated_manga_groups().await;
+
+        loop {
+            self.process_gui_commands().await;
+
+            if self.exiting {
+                break;
+            }
+        }
+    }
+
+    fn start(backend_send: BackendChannelSend, gui_recv: GuiChannelRecv) {
+        let runtime = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .context("Failed to create Tokio runtime.")
+            .unwrap();
+
+        let db_pool = runtime
+            .block_on(MangaUI::init_db())
+            .context("Failed to initialize DB pool.")
+            .unwrap();
+
+        DataStorage {
+            manga_groups: Vec::new(),
+            selected_group: Option::None,
+            group_to_delete: Option::None,
+            images_cache: HashMap::with_capacity(100),
+            cwd: std::env::current_dir()
+                .context("Unable to get CWD.")
+                .unwrap(),
+            db_pool: db_pool,
+            backend_send,
+            gui_recv,
+            exiting: false,
+        }
+        .start_backend(runtime);
+    }
+
+    async fn process_gui_commands(&mut self) {
+        while let Ok(cmd) = self
+            .gui_recv
+            .recv_timeout(std::time::Duration::from_millis(500))
+        {
+            match cmd {
+                GuiCommand::UpdateMangaGroups => self.update_manga_groups().await,
+                GuiCommand::CreateNewMangaGroup => self.create_new_manga_group().await,
+                GuiCommand::GetUpdatedMangaGroups => self.send_updated_manga_groups().await,
+                GuiCommand::DeleteMangaGroup(group) => group.delete_cascade(&self.db_pool).await,
+                GuiCommand::DeleteMangaEntry(entry) => entry.delete_cascade(&self.db_pool).await,
+                GuiCommand::DeleteImage(_) => todo!(),
+                GuiCommand::Exit => {
+                    self.exiting = true;
+                    break;
+                }
+            }
+        }
+    }
+    async fn send_updated_manga_groups(&self) {
+        self.backend_send
+            .send(BackendCommand::UpdateGroups(self.manga_groups.clone()))
+            .unwrap();
+    }
+
+    async fn create_new_manga_group(&mut self) {
+        sqlx::query!(r"INSERT INTO manga_groups DEFAULT VALUES")
+            .execute(&self.db_pool)
+            .await
+            .unwrap();
+        self.update_manga_groups().await;
+    }
+
+    async fn update_manga_groups(&mut self) {
+        self.manga_groups = sqlx::query_as!(
+            MangaGroup,
+            r"SELECT * FROM manga_groups ORDER BY added_on DESC, id DESC"
+        )
+        .fetch_all(&self.db_pool)
+        .await
+        .unwrap();
+
+        dbg!(&self.manga_groups);
+    }
 }
 
 impl eframe::App for MangaUI {
+    fn on_close_event(&mut self) -> bool {
+        self.gui_send.send(GuiCommand::Exit).unwrap();
+        true
+    }
+
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        self.process_backend_commands(ctx);
+
         egui::SidePanel::left("left_panel_manga_groups")
             .resizable(false)
             .exact_width(260.)
@@ -191,7 +295,7 @@ impl eframe::App for MangaUI {
                         });
                 }
 
-                // FIXME: this variable should not be here, but otherwise I get errors like
+                // FIXME: This variable should not be here, but otherwise I get errors like
                 // "cannot borrow mutably twice" or "cannot borrow immutable as mutable",
                 // because we borrow '&self' for loop, then in the closure we need to borrow
                 // '&mut self' for select_group() call.
@@ -256,27 +360,17 @@ impl eframe::App for MangaUI {
 
 impl MangaUI {
     fn create_new_manga_group(&mut self) {
-        self.async_runtime
-            .block_on(
-                sqlx::query!(r"INSERT INTO manga_groups DEFAULT VALUES").execute(&self.db_pool),
-            )
+        self.gui_send.send(GuiCommand::CreateNewMangaGroup).unwrap();
+        self.gui_send.send(GuiCommand::UpdateMangaGroups).unwrap();
+        self.gui_send
+            .send(GuiCommand::GetUpdatedMangaGroups)
             .unwrap();
-        self.refresh_manga_groups();
     }
 
     fn refresh_manga_groups(&mut self) {
-        self.manga_groups = self
-            .async_runtime
-            .block_on(
-                sqlx::query_as!(
-                    MangaGroup,
-                    r"SELECT * FROM manga_groups ORDER BY added_on DESC, id DESC"
-                )
-                .fetch_all(&self.db_pool),
-            )
+        self.gui_send
+            .send(GuiCommand::GetUpdatedMangaGroups)
             .unwrap();
-
-        dbg!(&self.manga_groups);
     }
 
     fn select_group(&mut self, group: MangaGroup) {
@@ -322,14 +416,31 @@ impl MangaUI {
         cc.egui_ctx.set_style(style);
         cc.egui_ctx.set_visuals(egui::Visuals::light());
 
+        let backend_recv_clone = self.backend_recv.clone();
+        let ctx_clone = cc.egui_ctx.clone();
+        // Since egui only calls update() when something has changed,
+        // and we do message processing there, no messages will be processed if
+        // there's no interaction from the user.
+        // To counter this, we use a clone of receiver and every 16ms check if
+        // there are messages from the backend, in a separate thread.
+        // TODO: is it possible to replace "every 16ms" with "every frame"?
+        std::thread::spawn(move || loop {
+            std::thread::sleep(std::time::Duration::from_millis(16));
+            if !backend_recv_clone.is_empty() {
+                ctx_clone.request_repaint();
+            }
+        });
+
         self
     }
 
     fn confirm_delete_group(&mut self) {
+        // Sanity check - we can't delete a group if no group was selected
         if self.group_to_delete.is_none() {
             return;
         }
 
+        // Unselect current group if we're deleting it
         if self
             .selected_group
             .as_ref()
@@ -338,9 +449,26 @@ impl MangaUI {
             self.selected_group = None;
         }
 
-        let group = self.group_to_delete.as_mut().unwrap();
-        group.delete_cascade(&self.async_runtime, &self.db_pool);
-        self.group_to_delete = None;
-        self.refresh_manga_groups();
+        self.gui_send
+            .send(GuiCommand::DeleteMangaGroup(
+                std::mem::replace(&mut self.group_to_delete, None).unwrap(),
+            ))
+            .unwrap();
+        self.gui_send.send(GuiCommand::UpdateMangaGroups).unwrap();
+        self.gui_send
+            .send(GuiCommand::GetUpdatedMangaGroups)
+            .unwrap();
+    }
+
+    fn process_backend_commands(&mut self, ctx: &egui::Context) {
+        println!("CHECKING");
+        while let Ok(cmd) = self.backend_recv.try_recv() {
+            dbg!(&cmd);
+            match cmd {
+                BackendCommand::UpdateGroups(groups) => self.manga_groups = groups,
+            }
+            println!("REPAINT");
+            ctx.request_repaint();
+        }
     }
 }
