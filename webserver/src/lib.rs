fa766a4c395cac4e4fb91332ac7ca08ed2a1258f
Your Name <you@example.com>
2025-12-02 20:11:28 +0700

    Add polyfill for crypto.randomUUID
    Apparently it works only on HTTPS or localhost

--- webserver/src/lib.rs
+++ webserver/src/lib.rs
@@ -272,6 +272,17 @@ async fn home_handler(State(state): State<Arc<RwLock<AppState>>>) -> Html<String
     <script>
         const initialState = {};
         let ws = null;
+
+        if (typeof crypto.randomUUID !== 'function')
+        {{
+            crypto.randomUUID = function () {{
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {{
+                    var r = Math.random() * 16 | 0,
+                        v = c === 'x' ? r : (r & 0x3 | 0x8);
+                    return v.toString(16);
+                }});
+            }};
+        }}
         let uuid = crypto.randomUUID();
         let lastMsg = "-";
 
